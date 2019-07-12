6906ad478e9ededa6a0dfea51b7ac9803b495e91
Sapna vishnoi <sapna18@navgurukul.org>
2019-07-12 21:00:54 +0530

    Update questions.js

--- Chankaya/questions.js
+++ Chankaya/questions.js
@@ -1,10 +1,38 @@
-const myQuestions = {
-    type : "integer",
-    correctAnswer:9
-};
-//class
+'use strict';
 
-class MyQuestions{
+const Joi = require('joi');
+const { Model } = require('./helpers');
+
+module.exports = class Question extends Model {
+
+   
+
+   static get joiSchema() {
+
+        return Joi.object({
+            id: Joi.number().integer().greater(0),
+            optionId: Joi.number().integer().greater(0).required(),
+            question : Joi.string(),
+            correct_answer: Joi.string().required(),
+            category: Joi.string().required()
+        });
+    }
+
+
+  static get relationMappings() {
     
-    
+    const User = require('./options');
+
+
+        return {
+            option: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Options,
+                join: {
+                    from: 'Question.id',
+                    to: 'options.optionId'
+                }
+            }
+        };
+    }
 }
